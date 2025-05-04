12a140a4f79f5db8e679f6d2fd89009c6d2417f3
ninayyyyy <merillo.junnalyn.dll@gmail.com>
2025-05-04 11:52:58 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import BookSearch from "./Components/BookSearch";
+import FileUploader from "./Components/FileUploader";
+
+
+function App() {
+  return (
+    <div className="App">
+      <BookSearch />
+      <FileUploader />
+    </div>
+  );
+}
+
+export default App;
